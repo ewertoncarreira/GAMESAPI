40dd1f2403201b5d729e8475ee7c1f93fd7f04b0
= <=>
2022-04-22 15:38:25 -0300

    Remove test console.log from Index

--- index.js
+++ index.js
@@ -8,9 +8,4 @@ app.use(bodyParser.json());
 const gamesController = require("./games/GamesController"); // USE GAME ROUTER
 app.use("/",gamesController);
 
-let a = {}
-a.type = "game"
-a.nome = 22
-console.log(a);
-
 app.listen(3000, () => console.log(`Servidor rodadndo na porta: ${3000}!`))
