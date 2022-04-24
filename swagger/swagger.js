d66d5660df1d93eadc13887169b3c13879dfbd6c
= <=>
2022-04-24 20:59:22 -0300

    New folder to swagger documentation

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,9 +1,8 @@
 
 const swaggerAutogen = require('swagger-autogen')();
 
-const outputFile = '.swagger_output.json';
-const endpointsFiles = ['./index.js'];
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['../index.js'];
 
 swaggerAutogen(outputFile, endpointsFiles,{},(res) => {
-    require("./index.js");
 })
